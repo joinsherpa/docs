08ac9dfda261d5512298003b18ab4fda884ca00e
quirkles <al.quirk@gmail.com>
2023-04-14 11:07:52 -0400

    docker deploy

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,5 +1,10 @@
-git_sha=$(git rev-parse main)
-docker buildx build --platform linux/amd64 -t usa-check-ui:"$git_sha" .
-docker tag usa-check-ui:"$git_sha" northamerica-northeast2-docker.pkg.dev/autobots-production/usa-check-ui/usa-check-ui:"$git_sha"
-docker push northamerica-northeast2-docker.pkg.dev/autobots-production/usa-check-ui/usa-check-ui:"$git_sha"
-gcloud run deploy usa-check-ui --image northamerica-northeast2-docker.pkg.dev/autobots-production/usa-check-ui/usa-check-ui:"$git_sha" --region northamerica-northeast2 --memory 128Mi --port 80 --allow-unauthenticated --project autobots-production
+cd ..
+
+GIT_SHA=$(git rev-parse main)
+APP_NAME=safecard-docs
+PROJECT=sherpa-autobots-dev
+
+docker buildx build --platform linux/amd64 -t "$APP_NAME":"$GIT_SHA" .
+docker tag "$APP_NAME":"$GIT_SHA" northamerica-northeast1-docker.pkg.dev/"$PROJECT"/"$APP_NAME"/"$APP_NAME":"$GIT_SHA"
+docker push northamerica-northeast1-docker.pkg.dev/"$PROJECT"/"$APP_NAME"/"$APP_NAME":"$GIT_SHA"
+gcloud run deploy "$APP_NAME" --image northamerica-northeast1-docker.pkg.dev/"$PROJECT"/"$APP_NAME"/"$APP_NAME":"$GIT_SHA" --region northamerica-northeast1 --memory 128Mi --port 80 --allow-unauthenticated --project "$PROJECT"
